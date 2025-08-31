d8070b9316914c3b9e82e49607a5d607190a071a
yuikawa-dev <yukawa71773@gmail.com>
2025-08-31 16:17:34 +0900

    エラーメッセージの装飾

--- resources/views/livewire/memos/create.blade.php
+++ resources/views/livewire/memos/create.blade.php
@@ -1,12 +1,26 @@
 <?php
 
-use function Livewire\Volt\{state};
+use function Livewire\Volt\{state, rules};
 use App\Models\Memo;
 
+// 変数宣言
 state(['title', 'body']);
+
+// バリデーションルール作成
+// validate() は rules() とセットで動くように設計されているため、
+// 特に明示的にルールを渡さなくても、rules() さえ定義しておけば自動で使ってくれる
+rules([
+    // タイトル → required = 必須項目、文字列、最大50文字
+    'title' => 'required|string|max:50',
+    // タイトル → required = 必須項目、文字列、最大2000文字
+    'body' => 'required|string|max:2000',
+]);
 
 // メモを保存する関数
 $store = function () {
+    // create処理前にチェック(rulesで書いてある内容を呼ぶ)
+    $this->validate();
+
     Memo::create([
         'title' => $this->title,
         'body' => $this->body,
@@ -24,13 +38,22 @@ $store = function () {
     {{-- ボタン押下時にstore関数を呼ぶ→PHPの中で$storeで作っている --}}
     <form wire:submit="store">
         <p>
-            <label for="title">タイトル</label><br>
+            <label for="title">タイトル</label>
+            @error('title')
+                {{-- エラーの場合呼び出されるif文のようなもの --}}
+                <span class="error">({{ $message }})</span>
+            @enderror
             {{-- wire:model="title" Modelカラムのtitleと結びつく --}}
+            <br>
             <input type="text" wire:model="title" id="title">
         </p>
         <p>
-            <label for="title">本文</label><br>
+            <label for="body">本文</label>
+            @error('body')
+                <span class="error">({{ $message }})</span>
+            @enderror
             {{-- wire:model="body" Modelカラムのbodyと結びつく --}}
+            <br>
             <textarea wire:model="body" id="body"></textarea>
         </p>
         <button type="submit">登録</button>
--- resources/views/livewire/memos/edit.blade.php
+++ resources/views/livewire/memos/edit.blade.php
@@ -1,13 +1,19 @@
 <?php
 
 // 取ってきて取得するからmountが必要
-use function Livewire\Volt\{state, mount};
+use function Livewire\Volt\{state, mount, rules};
 use App\Models\Memo;
 
 // state内部の変数定義は$がつかない
 // 初期表示させたい部分も変数を作る
 // $ がついていないのは、これは「変数を使う」のではなく、「変数名（文字列）を宣言している」だけ
 state(['memo', 'title', 'body']);
+
+// バリデーション定義
+rules([
+    'title' => 'required|string|max:50',
+    'body' => 'required|string|max:2000',
+]);
 
 // 開いたときにtitleとbodyそれぞれにDBのデータを表示してくれる
 // Memoモデルをmemoとして取得し、変数にいれる
@@ -22,6 +28,9 @@ mount(function (Memo $memo) {
 
 // 更新処理
 $update = function () {
+    // update処理の前にvalidate()でruleに書いたチェック処理を呼ぶ
+    $this->validate();
+
     // memo.phpのfillableで編集しても良い項目が安全に編集できるよう定義している
     $this->memo->update($this->all());
     return redirect()->route('memos.show', $this->memo);
@@ -36,14 +45,22 @@ $update = function () {
     {{-- ボタン押下時にstore関数を呼ぶ→PHPの中で$storeで作っている --}}
     <form wire:submit="update">
         <p>
-            <label for="title">タイトル</label><br>
+            <label for="title">タイトル</label>
+            @error('title')
+                <span class="error">({{ $message }})</span>
+            @enderror
             {{-- wire:model="title" Modelカラムのtitleと結びつく --}}
             {{-- mountの中の$memo->title --}}
+            <br>
             <input type="text" wire:model="title" id="title">
         </p>
         <p>
-            <label for="title">本文</label><br>
+            <label for="body">本文</label>
+            @error('body')
+                <span class="error">({{ $message }})</span>
+            @enderror
             {{-- wire:model="body" Modelカラムのbodyと結びつく --}}
+            <br>
             <textarea wire:model="body" id="body"></textarea>
         </p>
         <button type="submit">更新</button>
